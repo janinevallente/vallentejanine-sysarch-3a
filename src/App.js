f63df0ef7801f326724f07c120c0531433be770a
janinevallente <janinexvallente@gmail.com>
2023-03-23 20:03:25 +0800

    Third Commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import janine from './image/janine-image.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={janine} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Introduction:
         </p>
         <a
           className="App-link"
